f389ef49b8372f39c996bec5f231d8e9e68e3468
salman769 <salman.ali201518@gmail.com>
2021-08-07 15:03:35 +0500

    commit

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware'=>['auth.shopify','Role']], function () {
     Route::get('/', function () {
-        return view('/');
+        return view('dashboard');
     })->name('dashboard');
 
     // products
