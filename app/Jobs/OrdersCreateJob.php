e5f8a4fd69f381e4479f0896e2fc5f53cc287525
salman432 <salman.ali@tetralogicx.com>
2021-08-12 15:59:49 +0500

    commit

---
+++ app/Jobs/OrdersCreateJob.php
@@ -0,0 +1,66 @@
+<?php namespace App\Jobs;
+
+use App\Http\Controllers\OrderController;
+use App\Models\Preference;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Auth;
+use Osiset\ShopifyApp\Objects\Values\ShopDomain;
+use stdClass;
+
+class OrdersCreateJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Shop's myshopify domain
+     *
+     * @var ShopDomain|string
+     */
+    public $shopDomain;
+
+    /**
+     * The webhook data
+     *
+     * @var object
+     */
+    public $data;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param string   $shopDomain The shop's myshopify domain.
+     * @param stdClass $data       The webhook data (JSON decoded).
+     *
+     * @return void
+     */
+    public function __construct($shopDomain, $data)
+    {
+        $this->shopDomain = $shopDomain;
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // Convert domain
+        $this->shopDomain = ShopDomain::fromNative($this->shopDomain);
+        $shop = User::where('name', $this->shopDomain->toNative())->first();
+        $order_key = json_decode(json_encode($this->data), false);
+        $preferences = Preference::where('shop_id',Auth::user()->id)->first();
+        $location = $shop->api()->rest('GET', '/admin/locations.json');
+        $location = json_decode(json_encode($location));
+        $orderController = new OrderController;
+        $orderController->createShopifyOrders($order_key,$shop,$location,$preferences);
+        // Do what you wish with the data
+        // Access domain name as $this->shopDomain->toNative()
+    }
+}
