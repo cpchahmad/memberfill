75087ae7c96a62eeba82618143d5ff54de3bca3d
salman432 <salman.ali@tetralogicx.com>
2021-08-16 17:25:11 +0500

    commit

--- resources/views/users/groups/index.blade.php
+++ resources/views/users/groups/index.blade.php
@@ -37,11 +37,11 @@
     <div class="col-lg-12 col-md-12 p-4 bg-white">
 
         <div class="d-flex justify-content-between">
-            <div class="col-md-3 "><h6>Title</h6></div>
+            <div class="col-md-3 text-center"><h6>Title</h6></div>
             <div class="col-md-2 "><h6>Limit</h6></div>
             <div class="col-md-2"><h6>Products</h6></div>
             <div class="col-md-2"><h6>Action</h6></div>
-            <div class="col-md-3"><h6>Graph</h6></div>
+            <div class="col-md-3 text-center"><h6>Graph</h6></div>
 
         </div>
         @if (count($groups) > 0)
