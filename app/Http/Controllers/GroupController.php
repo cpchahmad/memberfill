69ef07bf38135256aae0c783567eb152d30690fd
salman432 <salman.ali@tetralogicx.com>
2021-08-31 18:34:21 +0500

    commit

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -40,7 +40,7 @@ class GroupController extends Controller
                     array_push($group_varient_qtn, $varient_qtn);
                 }
 
-                $groups->varient_qtn = $group_varient_qtn;
+                $group_varient_array = $group_varient_qtn;
 
                 $total_group_qtn = array_sum($group_varient_qtn);
                 array_push($group_sold_qtn, $total_group_qtn);
@@ -54,7 +54,7 @@ class GroupController extends Controller
             'graph_values' => $graph_values,
             'graph_labels' => $graph_labels,
             'preference'   => $preference,
-            'group->group_varient_qtn'   => $groups->group_varient_qtn,
+            'group->group_varient_qtn'   => $group_varient_array,
             'total_group_qtn'   => $total_group_qtn,
             'group_sold_qtn'   => $group_sold_qtn,
             'page_title' => 'groups'
